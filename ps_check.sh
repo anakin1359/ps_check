d75c542733143cdbf2dd3960a42b924fe7184b7b
anakin1359 <anakin1359@gmail.com>
2021-11-22 13:05:52 +0900

    first commit

---
+++ ps_check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ps_list=$(cat ./ps_list)
+
+for p in ${ps_list}; do
+	ps -C ${p} > /dev/null 2>&1 ;
+	if [ $? -eq 0 ] ; then
+		echo -e "\e[32mUP\e[m: ${p}" ;
+	else
+		echo -e "\e[31;5mDOWN\e[m: ${p}";
+	fi;
+done
+
